6d9669a8d6c3980b6e313354821f33069928d1bf
MorganClemens <morganclemens1@gmail.com>
2024-08-14 16:38:29 -0700

    Add settings page for global settings. Add a target framerate & redraw functionality. q

---
+++ settings.py
@@ -0,0 +1,11 @@
+#Controls the settings for the game
+
+class Settings:
+    #A class to store all settings for Alien Invasion
+
+    def __init__(self):
+        #Initialize the game's settings
+        #Screen Settings
+        self.screen_width  = 1920
+        self.screen_height = 1080
+        self.bg_color = (230, 230, 230)
