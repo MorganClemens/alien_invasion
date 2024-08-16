47a41a64261cef289bb9330a855e399aaf7fb512
MorganClemens <morganclemens1@gmail.com>
2024-08-16 10:36:36 -0700

    Add .gitignore. Build out ship movement functionality. Refactoring of ship.py, check_events in AlienInvasion class.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,43 +1,76 @@
-#Dependencies
+# Dependencies
 import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game asets and behavior"""
 
     def __init__(self):
-        """Initialize the game, and create game resources."""
+        # Initialize the game, and create game resources 
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height - 80
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # Create an instance of the ship
+        self.ship = Ship(self)
+
 
     def run_game(self):
-        """Start the main loop for the game."""
+        # Initialize main game loop
         while True:
-            #Game Loop
-            
-            #Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            # Set the target framerate to 60 fps
+            self.clock.tick(60)
+
+
+    def _check_events(self):
+        # Respond to keypresses and mouse events
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                    
-            #Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+    def _check_keydown_events(self, event):
+        # Respond to key presses
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyup_events(self, event):
+        # Respond to key releases
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
-            #Set the target framerate to 60 fps
-            self.clock.tick(60)
-            
+    def _update_screen(self):
+        # Update images on screen, and flip to new screen
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
-    #Make game instance, and run the game.
+    # Make game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
-#Controls the settings for the game
+# Controls the settings for the game
 
 class Settings:
-    #A class to store all settings for Alien Invasion
+    # A class to store all settings for Alien Invasion
 
     def __init__(self):
-        #Initialize the game's settings
-        #Screen Settings
-        self.screen_width  = 1920
-        self.screen_height = 1080
+        # Initialize the game's settings
+        # Screen Settings
+        self.screen_width  = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 3.5
