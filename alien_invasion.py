99096bfbf70a7811f59009a6c4b3aa15b1f55c0d
MorganClemens <morganclemens1@gmail.com>
2024-08-19 11:45:02 -0700

    Remove fullscreen support

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,10 +14,6 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height - 80
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
